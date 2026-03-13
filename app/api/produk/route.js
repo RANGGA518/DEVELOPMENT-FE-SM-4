a9718a1e6afbbf91f00b49cd7abea6330839e52b
RANGGA518 <ranggaardiansyah518@gmail.com>
2026-03-14 00:25:29 +0700

    Delete app directory

--- app/api/produk/route.js
+++
@@ -1,7 +0,0 @@
-import { connect } from "@/lib/db";
-
-export async function GET() {
-    const db = await connect()
-    const [rows] = await db.execute("SELECT * FROM produk")
-    return Response.json(rows)
-}
